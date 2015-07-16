81866ac940c16945d2116043002e53eb692abe7f
nigirock <nigirock124@gmail.com>
2015-07-16 12:19:34 +0300

    slider

--- js/app.js
+++ js/app.js
@@ -1,28 +1,20 @@
-(function($){
-    $(window).load(function(){
-        $("a[rel='m_PageScroll2id']").mPageScroll2id({
+$( document ).ready(function() {
+
+    var page = $("a[rel='m_PageScroll2id']");
+        page.mPageScroll2id({
             offset:120
         });
-    });
-    $('.owl-carousel').owlCarousel({
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items:1,
         margin:10,
         loop:true,
         nav:true,
         animateOut: 'fadeInRight',
         animateIn: 'fadeIn',
-        autoplay:true,
-        autoplayTimeout:3000,
-        autoplayHoverPause:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            480:{
-                items:1
-            },
-            1200:{
-                items:1
-            }
-        }
-    })
-})(jQuery);
+        autoPlay:false,
+        stopOnHover:true,
+        paginationSpeed : 2000,
+        rewindSpeed : 1000
+    });
+});
