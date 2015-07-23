abc5c0c510dccecf9cf7c4d1d77f07cf29fbe15f
nigirock <nigirock124@gmail.com>
2015-07-23 12:36:18 +0300

    контакты

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
 
     var page = $("a[rel='m_PageScroll2id']");
         page.mPageScroll2id({
-            offset:140
+            offset:120
         });
     var owl = $('.owl-carousel');
     owl.owlCarousel({
